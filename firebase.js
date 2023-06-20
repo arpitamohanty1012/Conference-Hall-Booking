08b223fec6d132b1ff3ba266b01abef0add6e31d
arpitamohanty1012 <114764893+arpitamohanty1012@users.noreply.github.com>
2023-06-20 23:33:28 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyBWCjnad1zYlGQphHtmDJw3IGLN_X-gelo',
+  authDomain: 'conference-hall-booking-75e43.firebaseapp.com',
+  projectId: 'conference-hall-booking-75e43',
+  storageBucket: 'conference-hall-booking-75e43.appspot.com',
+  messagingSenderId: '213378289692',
+  appId: '1:213378289692:web:321c28688b144783100fd9',
+};
+
+firebase.initializeApp(firebaseConfig);
